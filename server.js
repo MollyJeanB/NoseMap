75131f3f42a480274d9507ee734ebf10cadb4ede
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-01-30 13:39:58 -0800

    added test and set up travis

--- server.js
+++ server.js
@@ -2,3 +2,5 @@ const express = require("express");
 const app = express();
 app.use(express.static("public"));
 app.listen(process.env.PORT || 8080);
+
+module.exports = app;
---
+++ test/test-maps.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../server.js");
+
+chai.should();
+
+chai.use(chaiHttp);
+
+describe("index page", function() {
+  it("should exist", function() {
+    return chai
+      .request(app)
+      .get("/")
+      .then(function(res) {
+        res.should.have.status(200);
+      });
+  });
+});
