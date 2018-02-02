7ec2d21537e8741eff567ffa2bf46da709084702
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-02-02 15:04:44 -0800

    form submission update and styling

--- public/client.js
+++ public/client.js
@@ -1,45 +1,39 @@
 let map;
+let myPosition;
+let smellId = 2;
+// let popup, Popup;
 
 let MOCK_SMELLS = {
   mySmells: [
     {
+      id: 1,
       title: "poopy garbage",
       description: "diaper and yard waste smells from dumpster",
       category: "waste",
       intensity: 8,
-      publishedAt: 1470016976609,
+      publishedAt: "Wed Jan 31 2018 09:26:10 GMT-0800 (PST)",
       smellLocation: {
         lat: 45.5376723,
         lng: -122.6211201
       }
     },
     {
+      id: 2,
       title: "herbs and spices",
       description: "very calming, various kinds of tea and baked goods",
       category: "food and drink",
       intensity: 6,
-      publishedAt: 1470017976609,
+      publishedAt: "Fri Feb 02 2018 09:26:10 GMT-0800 (PST)",
       smellLocation: {
         lat: 45.5050628,
         lng: -122.6280184
-      }
-    },
-    {
-      title: "rain on asphalt",
-      description: "nostalgic smell of rain beginning",
-      catgory: "nature and environment",
-      intensity: 4,
-      publishedAt: 1470017976709,
-      smellLocation: {
-        lat: 45.5300958,
-        lng: -122.6169967
       }
     }
   ]
 };
 
 function getCurrentPositionSuccess(position) {
-  var myPosition = {
+  myPosition = {
     // lat: position.coords.latitude,
     // lng: position.coords.longitude
     lat: 45.5300958,
@@ -52,11 +46,12 @@ function getCurrentPositionSuccess(position) {
   });
 
   displayMapData(MOCK_SMELLS);
-  listenNewSmell(myPosition);
+  listenNewSmell();
 }
 
 function displayMapData(data) {
   for (var index in data.mySmells) {
+    let thisSmellId = data.mySmells[index].id;
     let smellTitle = data.mySmells[index].title;
     let smellDescription = data.mySmells[index].description;
     let smellCategory = data.mySmells[index].category;
@@ -68,7 +63,7 @@ function displayMapData(data) {
           <p>${smellDescription}</p>
           <p>${smellCategory}</p>
           <p>${smellCreated}</p>
-          <button type="submit">Edit Smell</button>
+          <button type="submit" onlick="listenUpdate(${thisSmellId})" class="edit-smell">Edit Smell</button>
           <button>Delete Smell</button>
           </div>`;
 
@@ -81,6 +76,13 @@ function displayMapData(data) {
       map: map,
       title: smellTitle
     });
+
+    // popup = new Popup(
+    //   new google.maps.LatLng(myPosition.lat, myPosition.lng),
+    //   document.getElementById("content")
+    // );
+    //
+    // popup.setMap(map);
 
     marker.setMap(map);
 
@@ -101,30 +103,51 @@ function addDataToMap(newSmellData) {
   MOCK_SMELLS.mySmells.push(newSmellData);
 }
 
-function listenNewSmell(smellPosition) {
-  $(".smell-form").submit(function(event) {
+function listenNewSmell() {
+  $(".smell-form").submit(event => {
     event.preventDefault();
     console.log("new smell submitted");
+    let thisDate = new Date();
+    let smellPosition = map.getCenter();
+    console.log(smellPosition);
     const smellData = {
+      id: smellId++,
       title: $(".smell-title").val(),
       description: $(".smell-description").val(),
-      category: $("#smell-category :radio:checked").val(),
+      category: $("input[name=category]:checked", "#smellsubmit").val(),
       intensity: $(".intensity-slider").val(),
-      // publishedAt: ,
+      publishedAt: thisDate,
       smellLocation: smellPosition
     };
+    console.log(smellData);
     addDataToMap(smellData);
     initMap();
+    $(".new-smell").addClass("hidden");
+    document.getElementById("showform").disabled = false;
   });
 }
 
-// function listenUpdate() {}
+//add find array element in ID function here
+
+function listenUpdate(currentSmellId) {}
 //
 // function listenDelete() {}
 
+function listenShowNewSmell() {
+  $(".show-new-smell-form").on("click", event => {
+    $(".new-smell").removeClass("hidden");
+    map.addListener("dragend", event => {
+      console.log(event);
+    });
+    document.getElementById("smellsubmit").reset();
+    document.getElementById("showform").disabled = true;
+    $(".bullseye").removeClass("hidden");
+  });
+}
+
 function handleApp() {
   initMap();
-  // listenUpdate();
+  listenShowNewSmell();
   // listenDelete();
 }
 
