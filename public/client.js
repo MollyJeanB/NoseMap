7bab4a379d005dda10f260451b378df9b277ac0d
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-02-04 23:24:51 -0800

    style updates and form correction

--- public/client.js
+++ public/client.js
@@ -3,6 +3,228 @@ let myPosition;
 let smellId = 2;
 // let popup, Popup;
 
+let mapStyle = [
+  {
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#242f3e"
+      }
+    ]
+  },
+  {
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#746855"
+      }
+    ]
+  },
+  {
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#242f3e"
+      }
+    ]
+  },
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#d59563"
+      }
+    ]
+  },
+  {
+    featureType: "administrative.neighborhood",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#d59563"
+      }
+    ]
+  },
+  {
+    featureType: "poi.business",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#263c3f"
+      }
+    ]
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#6b9a76"
+      }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#38414e"
+      }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#212a37"
+      }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#9ca5b3"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#746855"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#1f2835"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#f3d19c"
+      }
+    ]
+  },
+  {
+    featureType: "transit",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#2f3948"
+      }
+    ]
+  },
+  {
+    featureType: "transit.station",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#d59563"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#17263c"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#515c6d"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#17263c"
+      }
+    ]
+  }
+];
+
 let MOCK_SMELLS = {
   mySmells: [
     {
@@ -21,12 +243,24 @@ let MOCK_SMELLS = {
       id: 2,
       title: "herbs and spices",
       description: "very calming, various kinds of tea and baked goods",
-      category: "food and drink",
+      category: "food",
       intensity: 6,
       publishedAt: "Fri Feb 02 2018 09:26:10 GMT-0800 (PST)",
       smellLocation: {
         lat: 45.5050628,
         lng: -122.6280184
+      }
+    },
+    {
+      id: 4,
+      title: "rain on pines",
+      description: "the parl after a storm",
+      category: "nature",
+      intensity: 6,
+      publishedAt: "Sun Feb 04 2018 08:26:10 GMT-0800 (PST)",
+      smellLocation: {
+        lat: 45.52087156,
+        lng: -122.62561797
       }
     }
   ]
@@ -42,7 +276,8 @@ function getCurrentPositionSuccess(position) {
   console.log(myPosition);
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 13,
-    center: myPosition
+    center: myPosition,
+    styles: mapStyle
   });
 
   displayMapData(MOCK_SMELLS);
@@ -71,10 +306,13 @@ function displayMapData(data) {
       content: smellText
     });
 
+    const markerImage = "https://i.imgur.com/FVQb1CP.png";
+
     const marker = new google.maps.Marker({
       position: smellPosition,
       map: map,
-      title: smellTitle
+      title: smellTitle,
+      icon: markerImage
     });
 
     // popup = new Popup(
@@ -124,12 +362,13 @@ function listenNewSmell() {
     initMap();
     $(".new-smell").addClass("hidden");
     document.getElementById("showform").disabled = false;
+    $(".location").addClass("hidden");
   });
 }
 
 //add find array element in ID function here
 
-function listenUpdate(currentSmellId) {}
+// function listenUpdate(currentSmellId) {}
 //
 // function listenDelete() {}
 
@@ -141,13 +380,23 @@ function listenShowNewSmell() {
     });
     document.getElementById("smellsubmit").reset();
     document.getElementById("showform").disabled = true;
-    $(".bullseye").removeClass("hidden");
+    $(".location").removeClass("hidden");
+  });
+}
+
+function listenInfoX() {
+  $(".close").on("click", event => {
+    $(".new-smell").addClass("hidden");
+    document.getElementById("showform").disabled = false;
+    $(".location").addClass("hidden");
   });
 }
 
 function handleApp() {
   initMap();
   listenShowNewSmell();
+  listenInfoX();
+  // listenUpdate();
   // listenDelete();
 }
 
